18db064145a268d8eddd4a705241d24564b811fd
bruceagle818 <bruceagle818@gmail.com>
2016-11-06 13:45:37 -0800

    admin index table

--- app/controllers/admin_control_controller.rb
+++ app/controllers/admin_control_controller.rb
@@ -2,6 +2,7 @@ class AdminControlController < ApplicationController
 
   http_basic_authenticate_with :name => ENV["CP_USER"], :password => ENV["CP_PASSWORD"]
   def index
+    @user = User.all
   end
 
   def new
