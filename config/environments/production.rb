a5234737d5a9c084c33c369a0d20aa8b673913ce
ConnorHovanessian <chovanes@sfu.ca>
2016-11-14 15:21:48 -0800

    Updated Environments for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
 
     config.action_mailer.default_url_options = { :host => "localhost:5000" }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
