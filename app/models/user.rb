6b24eb6f2941cf4a09006823ea463d744be64870
ConnorHovanessian <chovanes@sfu.ca>
2016-11-14 02:22:53 -0800

    Updated user mailers for password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save :default_values
 
 
@@ -46,5 +46,17 @@ class User < ActiveRecord::Base
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+  # Sets the password reset attributes.
+def create_reset_digest
+  self.reset_token = User.new_token
+  update_attribute(:reset_digest,  User.digest(reset_token))
+  update_attribute(:reset_sent_at, Time.zone.now)
+end
+
+# Sends password reset email.
+ def send_password_reset_email
+   UserMailer.password_reset(self).deliver_now
+ end
 
 end
