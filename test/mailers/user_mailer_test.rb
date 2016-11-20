e35d08630ed308d2c510898be8de2f01b2082f02
lydiazheng <lydiazheng0928@gmail.com>
2016-11-19 19:47:13 -0800

    add password reset test

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,5 +1,14 @@
 require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
-
+	 test "password_reset" do
+		user = users(:john)
+		user.reset_token = User.new_token
+		mail = UserMailer.password_reset(user)
+		assert_equal "Password reset", mail.subject
+    	assert_equal [user.email], mail.to
+    	assert_equal ["noreply@UrbanHunt.com"], mail.from
+    	assert_match user.reset_token,        mail.body.encoded
+    	assert_match CGI.escape(user.email),  mail.body.encoded
+	end
 end
