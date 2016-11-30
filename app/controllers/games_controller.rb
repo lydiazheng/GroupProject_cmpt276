fa00a09e3134f9fa62dabc5a1bb0ce19b13a963e
lydiazheng <lydiazheng0928@gmail.com>
2016-11-29 22:38:15 -0800

    add hint and fix the controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -49,18 +49,14 @@ class GamesController < ApplicationController
     redirect_to games_url 
   end
 
-  def add_histories
+  def history
     @game = Game.new
     @user = params[:user_id]
-    @location = params[:location]
-    @hint1 = params[:hint1_used]
-    @hint2 = params[:hint2_used]
+
     
     @location.update_attribute(:user_id, current_user.id)
     @location.update_attribute(:timestamp, DateTime.now.in_time_zone("Pacific Time (US & Canada)"))
 
-
-    redirect_to controller: 'users', action: 'show', id: @current_user.id
   end
 
 
--- app/models/game_history.rb
+++ app/models/game_history.rb
@@ -1,2 +1,4 @@
 class GameHistory < ActiveRecord::Base
+	belongs_to :user
+	
 end
