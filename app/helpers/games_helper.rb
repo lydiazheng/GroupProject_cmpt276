ddc7a4ddccd7fa598962c868b34fcd09392179db
lydiazheng <lydiazheng0928@gmail.com>
2016-11-29 22:58:05 -0800

    add address and duration to game history

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,8 +1,13 @@
 module GamesHelper
 	def get_game_name(gid)
 		return Game.find_by(id: gid).title
-
 	end
 
+	def get_game_address(g_address)
+		return Game.find_by(id: g_address).address
+	end
 
+	def get_game_duration(g_duration)
+		return Game.find_by(id: g_duration).duration
+	end
 end
