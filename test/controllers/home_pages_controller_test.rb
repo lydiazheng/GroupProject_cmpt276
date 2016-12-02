d453a3ca4f6ab9316b1be25a0d04afa3e632e820
郑昊 <zhwtf123@gmail.com>
2016-12-01 17:53:28 -0800

    locationcontrollertest

--- test/controllers/home_pages_controller_test.rb
+++ test/controllers/home_pages_controller_test.rb
@@ -6,6 +6,14 @@ class HomePagesControllerTest < ActionController::TestCase
   	assert_routing '/users/1', {controller: "users", action: "show", id: "1"}
 end
 
+  test "should route to user index" do
+  	assert_routing '/users', {controller: "users", action: "index"}
+end
+
+test "should route to user edit" do
+  	assert_routing '/users/1/edit', {controller: "users", action: "edit", id: "1"}
+end
+
   test "should get home" do
     get :home
     assert_response :success
