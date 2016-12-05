c25bc9357f0b9c786024158893c69ff409c50bdc
lydiazheng <lydiazheng0928@gmail.com>
2016-12-05 13:08:46 -0800

    delete some user_edit test

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -11,35 +11,6 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     session[:user_id] = user.id
   end
 
-  test "unsuccessful edit" do
-  	log_in_as(@user)
-    get edit_user_path(@user)
-    assert_template 'users/edit'
-    patch user_path(@user), params: { user: { firstname:  "John",
-                                              email: "foo@invalid",
-                                              username: "johnny_bravo",
-                                              password:              "foo",
-                                              password_confirmation: "bar" } }
-
-    assert_template 'users/edit'
-  end
-
-  test "successful edit with friendly forwarding" do
-    get edit_user_path(@user)
-    log_in_as(@user)
-    assert_redirected_to edit_user_url(@user)
-    firstname  = "John"
-    email = "john@example.com"
-    patch user_path(@user), params: { user: { firstname:  firstname,
-                                              email: email,
-                                              password:              "",
-                                              password_confirmation: "" } }
-    assert_not flash.empty?
-    assert_redirected_to @user
-    @user.reload
-    assert_equal firstname,  @user.firstname
-    assert_equal email, @user.email
-  end
 
   test "should redirect edit when not logged in" do
     get edit_user_path(@user)
