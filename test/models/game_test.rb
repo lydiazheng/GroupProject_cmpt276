18ac7f46e9c43fab418396971ae1f28739f6a083
lydiazheng <lydiazheng0928@gmail.com>
2016-12-05 17:52:15 -0800

    fix game model test

--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -6,18 +6,20 @@ class GameTest < ActiveSupport::TestCase
   # end
   def setup
     @game = games(:SFU_library)
+    @game.starts_at_time = 12-06-00
+    @game.starts_at_date = 2016-12-05
   end
 
   test "the game should be valid" do
   	assert @game.valid?, @game.errors.full_messages.inspect
   end
 
-  test "the longitude can be absent" do
+  test "the longitude cannot be absent" do
     @game.longitude = "     "
-    assert @game.valid?
+    assert_not @game.valid?
   end
 
-  test "the longitude cannot have special symbol" do
+  test "the longitude must be only in integer" do
     valid_longitudes = [
       "aaaa",
       "aaAA",
@@ -25,16 +27,16 @@ class GameTest < ActiveSupport::TestCase
       "!!112"]
     valid_longitudes.each do |valid_longitude|
      @game.longitude = valid_longitude
-     assert @game.valid?, "#{valid_longitude.inspect} should be valid"
+     assert_not @game.valid?, "#{valid_longitude.inspect} should be valid"
    end
  end
   
-  test "the latitude can be absent" do
+  test "the latitude cannot be absent" do
     @game.latitude = "     "
-    assert @game.valid?
+    assert_not @game.valid?
    end
 
-  test "the latitude can be any type" do
+  test "the latitude must be only in integer" do
     valid_latitudes = [
       "aaaa",
       "aaAA",
@@ -42,35 +44,35 @@ class GameTest < ActiveSupport::TestCase
       "!!112"]
     valid_latitudes.each do |valid_latitude|
      @game.latitude = valid_latitude
-     assert @game.valid?, "#{valid_latitude.inspect} should be valid"
+     assert_not @game.valid?, "#{valid_latitude.inspect} should be valid"
    end
  end
 
- test "the address can be any type" do
-  valid_addresses = [
-    "abcdff",
-    "12 sdf",
-    "!!12 ASD",
-    "      "]
-  valid_addresses.each do |valid_address|
-    @game.address = valid_address
-    assert @game.valid?, "#{valid_address.inspect} should be valid"
+
+
+  test "the title can not be absent" do
+    @game.title = "    "
+    assert_not @game.valid?
+    end
+
+  test "title should not be too long" do
+    @game.title = "a" * 65
+    assert_not @game.valid?
   end
-end
-  
-  test "the title can be any type" do
+
+  test "the title cannot be a combination of numebr, letter(uppercase or lowercase) and symbol" do
   valid_titles = [
-    "abcdff",
+    "asdfsd",
     "12 sdf",
-    "!!12 ASD",
-    "      "]
+    "12"]
   valid_titles.each do |valid_title|
     @game.title = valid_title
-    assert @game.valid?, "#{valid_title.inspect} should be valid"
+    assert_not @game.valid?, "#{valid_title.inspect} should be valid"
   end
 end
 
-  test "the duration can be any type" do
+
+  test "the duration must be integer" do
   valid_durations = [
     "abcdff",
     "12 sdf",
@@ -79,10 +81,20 @@ end
     "+-*/ ASD dfsc"]
   valid_durations.each do |valid_duration|
     @game.duration = valid_duration
-    assert @game.valid?, "#{valid_duration.inspect} should be valid"
+    assert_not @game.valid?, "#{valid_duration.inspect} should be valid"
   end
 end
 
+
+  test "duration should be larger than 5 miniutes" do
+    @game.duration = 4
+    assert_not @game.valid?
+  end
+
+  test "duration should be less than 120 miniutes" do
+    @game.duration = 121
+    assert_not @game.valid?
+  end
 
 
 
